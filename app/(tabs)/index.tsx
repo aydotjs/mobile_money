9be3aecfd89b27715284bca7910a60d495c898d1
aydotjs <ayobami66@gmail.com>
2024-10-22 09:14:42 +0100

    core components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+// Default export for Whatever component
+export default function Whatever() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Whats up</Text>
+      <HomeScreen/>
     </View>
   );
 }
+
+// Named export for HomeScreen component
+export function HomeScreen() {
+  return <Text>What is going on here for God's sake</Text>;
+}
+
 
 const styles = StyleSheet.create({
   container: {
