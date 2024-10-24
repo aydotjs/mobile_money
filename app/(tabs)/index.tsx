1e761df6171a622944c066bacf8257f8022248e5
aydotjs <ayobami66@gmail.com>
2024-10-24 16:36:34 +0100

    basic inline styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,10 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-// Default export for Whatever component
-export default function Whatever() {
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+// Default export for r component
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Whats up</Text>
-      <HomeScreen/>
+    <View style={{ backgroundColor: "red", width: 200, height: 300 }}>
+      <Text style={{ color: "white" }}>Whats up</Text>
     </View>
   );
 }
-
-// Named export for HomeScreen component
-export function HomeScreen() {
-  return <Text>What is going on here for God's sake</Text>;
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
