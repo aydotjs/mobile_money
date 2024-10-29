2045e2925c92a63c6b857a95252a2f1bca61ac38
aydotjs <ayobami66@gmail.com>
2024-10-29 09:17:46 +0100

    modal usage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,15 +7,18 @@ import {
   ScrollView,
   Button,
   Pressable,
+  Modal,
 } from "react-native";
 
 const myPicture = require("../../assets/images/IMG_001-modified.png");
 
 export default function HomeScreen() {
-  // State variable to hold the background color
+  // State variables to hold the visual feedback properties
   const [bgColor, setBgColor] = useState("white");
   const [textColor, setTextColor] = useState("black");
   const [width, setWidth] = useState(300);
+  const [imageOpacity, setImageOpacity] = useState(1); // For onPressIn and onPressOut
+  const [modalStatus, setModalStatus] = useState(false);
 
   // Function to change background color on button press
   function handleClick() {
@@ -23,11 +26,26 @@ export default function HomeScreen() {
     const newTextColor = textColor === "black" ? "white" : "black";
     setBgColor(newColor);
     setTextColor(newTextColor);
+    setModalStatus(true);
   }
+
   function handlePress() {
     const newWidth = width === 300 ? 500 : 300;
-    setWidth(newWidth)
+    setWidth(newWidth);
   }
+
+  function handleOnPressIn() {
+    setImageOpacity(0.5); // Reduces opacity to 50% on press
+  }
+
+  function handleOnPressOut() {
+    setImageOpacity(1); // Resets opacity back to 100%
+  }
+
+  function handleLongPress() {
+    setBgColor(bgColor === "white" ? "lightcoral" : "white"); // Change background color on long press
+  }
+
   return (
     <View style={[styles.container, { backgroundColor: bgColor }]}>
       <Button
@@ -36,13 +54,26 @@ export default function HomeScreen() {
         onPress={handleClick}
       />
       <ScrollView>
-        <Pressable onPress={handlePress}>
+        <Pressable
+          onPress={handlePress}
+          onPressIn={handleOnPressIn}
+          onPressOut={handleOnPressOut}
+          onLongPress={handleLongPress}
+        >
           <Image
             source={{ uri: "https://picsum.photos/200" }}
-            style={{ width: width, height: 300 }}
+            style={{ width: width, height: 300, opacity: imageOpacity }}
           />
         </Pressable>
-
+        <Modal
+          visible={modalStatus}
+          onRequestClose={() => setModalStatus(false)}
+          animationType={"slide"}
+          presentationStyle={"formSheet"}
+        >
+          <Text>This is the content of the Modal</Text>
+          <Button title="Cancel" onPress={() => setModalStatus(false)} />
+        </Modal>
         <Text style={{ color: textColor }}>
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
